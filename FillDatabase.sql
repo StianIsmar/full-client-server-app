a1c7c43ca3e76d3b30b642034cd4dade12ac4d42
StianIsmar <41960375+StianIsmar@users.noreply.github.com>
2019-02-21 16:00:59 +0100

    Can now do database query in index.js

---
+++ FillDatabase.sql
@@ -0,0 +1,10 @@
+use react_sql;
+
+INSERT INTO products (idproducts, name, price)
+VALUES (2, 'PS3', 1999);
+
+INSERT INTO products (idproducts, name, price)
+VALUES (3, 'Wii', 2500);
+
+INSERT INTO products (idproducts, name, price)
+VALUES (4, 'Gameboy Advance', 4000);
--- product-server/index.js
+++ product-server/index.js
@@ -7,15 +7,18 @@ const mysql = require('mysql');
 
 const app = express();
 // Query
-const SELECT_ALL_PRODUCTS_QUERY = 'SELECT * FROM products';
+const SELECT_ALL_PRODUCTS_QUERY = 'SELECT * FROM react_sql.products';
 
 // Gives instance of our connection
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'theo',
-    database: 'react_sql'
+    password: 'kflip44',
+    database: 'react_sql',
+    port: 3306
 });
+connection.connect();
+// console.log(connection)
 
 connection.connect(err => {
     if (err) {
@@ -28,18 +31,29 @@ app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('go to /products to see products')
-
 });
+
+app.get('/empty', (req, res) => {
+    res.send('Empty')
+});
+
+/*app.get('/products/add', (req, res) => {
+    const { name, price } = req.query;
+    console.log(name, price)
+    res.send('adding product')
+})*/
 
 app.get('/products', (req, res) => {
     connection.query(SELECT_ALL_PRODUCTS_QUERY, (err, result) => {
         if (err) {
+            console.log("ERRRRRORRRR")
             return res.send(err)
         }
         else {
-            return res.json({
-                data: results
-            })
+            console.log(res.json({ data: result }))
+            //return res.json({
+            //  data: results
+            //})
         }
     })
 });
